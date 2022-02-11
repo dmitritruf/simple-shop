e9f14658afafe9f0413a25a0ab005285a6ce1371
dmitritruf <gttim2@gmail.com>
2022-02-11 11:13:21 +0200

    move to Service user

--- server/service/userService.ts
+++ server/service/userService.ts
@@ -1,3 +1,8 @@
-class UserService {}
+import model from '../models/model';
+class UserService {
+  async registration(email, password) {
+    const candidate = model.User.findOne({ where: { email } });
+  }
+}
 
 export default new UserService();
